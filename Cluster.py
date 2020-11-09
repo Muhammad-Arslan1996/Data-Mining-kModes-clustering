4bb592249c1eee22f96caea32035f49a635c5e01
marslan <muhammadarslan1502@gmail.com>
2020-11-08 20:41:55 -0800

    faster implementation

--- Cluster.py
+++ Cluster.py
@@ -3,11 +3,16 @@ import numpy as np
 from random import choice
 import math
 
-def hamming_distance_and_choose_cluster(row, cluster_modes):
-    distances = np.array([])
-    for cluster_mode in cluster_modes:
-        distances = np.append(distances, row.eq(cluster_mode).value_counts().get(False, 0))
-    return np.argmin(distances)
+def hamming_distance_and_choose_cluster(data, cluster_modes):
+    clusters = []
+    for idx, cluster_mode in enumerate(cluster_modes):
+        df = data.iloc[:, 0:22]
+        clusters.append('c' + str(idx))
+        data['c' + str(idx)] = df.ne(cluster_mode).sum(1)
+    data['cluster'] = (data.loc[:, clusters].idxmin(axis = 1))
+    data['cluster'] = data['cluster'].str[1:].astype(int)
+    data = data.drop(columns=clusters)
+    return data
 
 #calculate cluster modes after cluster assignment
 def calculate_cluster_modes(data, number_of_clusters):
@@ -35,15 +40,17 @@ def k_mode_clustering(data, number_of_clusters):
     df_with_cluster = data.copy()
     df_with_cluster.drop('p', 1, inplace=True)
     
-    #for random centroids, remove the rows with missing values
-    data_without_missing_values = df_with_cluster[df_with_cluster['e.1'] != '?'].reset_index()
-    
     #chose random centroids
-    rows = len(data_without_missing_values)
-    for i in range(number_of_clusters):
+    rows = len(df_with_cluster)
+    i = 0
+    while i < number_of_clusters:
         rand_index = choice([i for i in range(rows) if i not in already_chosen_clusters])
+        value = df_with_cluster.loc[rand_index]
+        if value['e.1'] == '?':
+            continue
         already_chosen_clusters.append(rand_index)
-        cluster_centroids.append(data_without_missing_values.loc[rand_index])
+        cluster_centroids.append(df_with_cluster.loc[rand_index])
+        i += 1
     new_cluster_centroids = []
     
     iteration = 0
@@ -51,7 +58,8 @@ def k_mode_clustering(data, number_of_clusters):
         print('On iteration number: {}'.format(iteration))
         new_cluster_centroids = cluster_centroids
         df_with_cluster.drop('cluster', 1, inplace=True, errors='ignore')
-        df_with_cluster['cluster'] = df_with_cluster.apply(hamming_distance_and_choose_cluster, args=[new_cluster_centroids], axis=1)
+        #df_with_cluster['cluster'] = df_with_cluster.apply(hamming_distance_and_choose_cluster, args=[new_cluster_centroids], axis=1)
+        df_with_cluster = hamming_distance_and_choose_cluster(df_with_cluster, new_cluster_centroids)
         cluster_centroids = calculate_cluster_modes(df_with_cluster, number_of_clusters)
         
         if(iteration == 0):
@@ -59,7 +67,6 @@ def k_mode_clustering(data, number_of_clusters):
                 mask = (df_with_cluster['e.1'] == '?') & (df_with_cluster['cluster'] == i)
                 df_with_cluster.loc[mask, 'e.1'] = df_with_cluster.loc[acc]['e.1']
             
-        print(len(df_with_cluster[df_with_cluster['e.1'] == '?']))
         iteration += 1
 
     df_with_cluster.to_csv('prediction.csv')
@@ -68,8 +75,7 @@ def k_mode_clustering(data, number_of_clusters):
 
 def main():
     data = pd.read_csv('./agaricus-lepiota.data')
-    result = k_mode_clustering(data, 8)
-    print(result['e.1'].unique())
+    result = k_mode_clustering(data, 13)
 
 
 if __name__ == "__main__":
